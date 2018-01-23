baa0b4ff97a27f3bbcaa876f1f167d87c5d81cd9
GammonDTT <30335266+GammonDTT@users.noreply.github.com>
2018-01-23 18:55:49 +0800

    Add files via upload

--- CraneProximity_r3.py
+++ CraneProximity_r3.py
@@ -1,10 +1,18 @@
 from bluepy.btle import Scanner, DefaultDelegate
+import csv
 import RPi.GPIO as GPIO
 
-beaconAddr = ["c8:03:83:4f:aa:42"]
-beaconThreshold = -40
+# Variables
+try:
+    with open('/home/pi/Documents/Proximity_Detection/beaconReg.csv', 'r') as f:  # Import registered beacons
+        beaconListFull = list(csv.reader(f))
+        beaconAddr = [item[1] for item in beaconListFull]
+    with open('/home/pi/Documents/Proximity_Detection/threshold.txt', 'r') as g:
+        beaconThreshold = int(g.readline())
+except (IOError, EOFError, TimeoutError) as e:
+        GPIO.cleanup()
 
-# GPIO preset TODO
+# GPIO preset
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 
@@ -12,7 +20,7 @@ GPIO.setup(7, GPIO.OUT)
 alarm = False
 counter = 0
 
-# Def alarm on/off TODO
+# Def alarm on/off
 def alarmOn():
     GPIO.output(7,True)
             
@@ -27,10 +35,10 @@ class ScanDelegate(DefaultDelegate):
 while True:
     try:
         scanner = Scanner().withDelegate(ScanDelegate())
-        devices = scanner.scan(0.5) # Scans for n seconds
+        devices = scanner.scan(2) # Scans for n seconds
         beaconRssiList = []
         for dev in devices:
-            if (beaconAddr in dev.addr):
+            if (dev.addr in beaconAddr):
                 print("Device {}, RSSI={} dB".format(dev.addr, dev.rssi))
                 beaconRssiList.append(dev.rssi)
         if (max(beaconRssiList, default=-999)>beaconThreshold):
@@ -40,12 +48,12 @@ while True:
                 alarm = True
         elif (not beaconRssiList) or (max(beaconRssiList) < beaconThreshold):
             counter += 1
-            if counter > 25:
+            if counter > 2:
                 counter = 0
                 alarmOff()
                 alarm = False
     except (KeyboardInterrupt) as k:
         GPIO.cleanup()
         quit()
-    except (IOError, EOFError, TimeoutError) as e:
+    except (IOError, EOFError, TimeoutError) as l:
         GPIO.cleanup()
